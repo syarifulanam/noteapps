4912c75116a45849834151f01cbde67db91f0450
syarifulanam <em.syariful.anam@gmail.com>
2026-02-19 16:41:17 +0700

    tambah html create

--- src/main/java/com/syariful/noteapps/controller/NoteController.java
+++ src/main/java/com/syariful/noteapps/controller/NoteController.java
@@ -1,13 +1,20 @@
 package com.syariful.noteapps.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/notes")
 public class NoteController {
 
+    @GetMapping
     public String index() {
         return "notes/index";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "notes/create";
     }
 }
